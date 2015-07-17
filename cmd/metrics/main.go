071ca161eb4410f0b05a2b27e83b38e3629f8c8c
Ed King <ed@cloudcredo.com>
2015-07-17 13:47:54 +0100

    Get password from redis config
    Signed-off-by: Stev Witzel <switzel@pivotal.io>

--- cmd/metrics/main.go
+++ cmd/metrics/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cloudfoundry/dropsonde"
 	"github.com/cloudfoundry/dropsonde/metrics"
 	"github.com/pivotal-cf/cf-redis-broker/redis/client"
+	"github.com/pivotal-cf/cf-redis-broker/redisconf"
 )
 
 var (
@@ -17,13 +18,13 @@ var (
 	metronPort    int
 	redisHost     string
 	redisPort     int
-	redisPassword string
+	redisConfPath string
 )
 
 func main() {
+	flag.StringVar(&redisConfPath, "redis-config", "", "Path to redis config file")
 	flag.StringVar(&redisHost, "redis-host", "localhost", "Redis host")
 	flag.IntVar(&redisPort, "redis-port", 6379, "Redis port")
-	flag.StringVar(&redisPassword, "redis-password", "", "Redis password")
 	flag.StringVar(&metronHost, "metron-host", "localhost", "Metron host")
 	flag.IntVar(&metronPort, "metron-port", 3457, "Metron port")
 
@@ -40,19 +41,36 @@ func main() {
 	}
 }
 
+func password() (string, error) {
+	conf, err := redisconf.Load(redisConfPath)
+	if err != nil {
+		return "", err
+	}
+
+	return conf.Get("requirepass"), nil
+}
+
 func capture() {
+	pwd, err := password()
+	if err != nil {
+		log.Println("Error getting password from redis config: " + err.Error())
+		return
+	}
+
 	client, err := client.Connect(
 		client.Host(redisHost),
 		client.Port(redisPort),
-		client.Password(redisPassword),
+		client.Password(pwd),
 	)
 	if err != nil {
 		log.Println("Error connecting to Redis: " + err.Error())
+		return
 	}
 
 	info, err := client.Info()
 	if err != nil {
 		log.Println("Error getting info form Redis: " + err.Error())
+		return
 	}
 
 	client.Disconnect()
@@ -60,11 +78,13 @@ func capture() {
 	cpu, err := strconv.ParseFloat(info["used_cpu_sys"], 64)
 	if err != nil {
 		log.Println("Error getting info form Redis: " + err.Error())
+		return
 	}
 
 	fmt.Printf("CPU: %.2f\n", cpu)
 
 	if err = metrics.SendValue("cpu", cpu, "Load"); err != nil {
 		log.Println("Error emitting metric: " + err.Error())
+		return
 	}
 }
